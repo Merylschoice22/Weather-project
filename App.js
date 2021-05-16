232652b3789db0550980e656d70296c7ca08f67d
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-16 19:16:29 +0200

    Added basic info to the files from documents. HTML, CSS, and some Javascript

---
+++ App.js
@@ -0,0 +1,7 @@
+// Go to this URL and register https://openweathermap.org/appid
+// Get your API KEY (appid)
+
+const APIkey = "56ff6869ae6e4a328054f75904f02b09";
+const baseURL = "https://api.weatherbit.io/v2.0/current?";
+// const latitude = 35.7796;
+// const longitude = -78.6382;
